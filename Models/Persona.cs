02fcd1eedc076258e3ef204325ca040cd3618287
Bustosexe <exequiel.bm6@gmail.com>
2026-05-05 23:20:52 -0300

    Entrega TP2 - Blazor WebAssembly

---
+++ Models/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace TP2_Programacion_Exequiel.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Domicilio { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public string RutaImagen { get; set; } = string.Empty;
+    }
+}
